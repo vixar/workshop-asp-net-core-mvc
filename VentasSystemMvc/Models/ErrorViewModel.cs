a32cc71c5ad524a4de38623d4e4d02c7f0f83719
Vickry Santana <Vickry.Santana@gmail.com>
2018-10-25 21:33:53 -0400

    Add project files.

---
+++ VentasSystemMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VentasSystemMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
